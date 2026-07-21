6f6c7f8f9889137953284bb42f432939680c98e1
Aayush Kumar Gupta <ayushkmr2509@gmail.com>
2026-07-21 19:58:51 +0530

    Time: 13 ms (8.31%) | Memory: 50.1 MB (92.32%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
@@ -7,38 +7,33 @@ int [][]dp;
             return 1;
         }
         dp = new int[n][2];
-        for(int []it:dp)
+        dp[0][1]=1;
+        dp[0][0]=1;
+        for(int i=1;i<n;i++)
         {
-            Arrays.fill(it,-1);
+            if(arr[i-1]>arr[i])
+            {
+                dp[i][1]= 1+dp[i-1][0];
+                dp[i][0]=1;
+            }
+            else if(arr[i-1]<arr[i])
+            {
+                dp[i][0]=1+dp[i-1][1];
+                dp[i][1]=1;
+            }
+            else
+            {
+                dp[i][0]=1;
+                dp[i][1]=1;
+            }
         }
         int maxi = 1;
         for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, count(arr, i, 0));
-            maxi = Math.max(maxi, count(arr, i, 1));
+            maxi = Math.max(maxi, dp[i][0]);
+            maxi = Math.max(maxi, dp[i][1]);
         }
         return maxi;
-    }
-
-    private int count(int[] arr, int index, int up) {
-        if (index == 0) {
-            return 1;
-        }
-        int take=1;
-        if(dp[index][up]!=-1)
-        {
-            return dp[index][up];
-        }
-        if (up == 0) {
-            if (arr[index - 1] < arr[index]) {
-                take = 1 + count(arr, index - 1, 1);
-            }
-        } else {
-            if (arr[index - 1] > arr[index]) {
-                take = 1 + count(arr, index - 1, 0);
-            }
-        }
-            return dp[index][up]=take;
-        
+    
     }
 
 }
