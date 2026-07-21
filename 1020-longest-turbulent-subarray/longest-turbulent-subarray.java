afd9f50b6a1eac3a6512cfd8f805410c54a03dbf
Aayush Kumar Gupta <ayushkmr2509@gmail.com>
2026-07-21 20:03:27 +0530

    Time: 10 ms (9.76%) | Memory: 50.3 MB (91.28%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
@@ -1,37 +1,38 @@
 class Solution {
     int n;
-int [][]dp;
+int []dp;
     public int maxTurbulenceSize(int[] arr) {
         n = arr.length;
         if (n == 1) {
             return 1;
         }
-        dp = new int[n][2];
-        dp[0][1]=1;
-        dp[0][0]=1;
+        dp = new int[2];
+        dp[0] =1;
+        dp[1] =1;
+        int maxi =1;
         for(int i=1;i<n;i++)
         {
+            int []curr = new int[2];
             if(arr[i-1]>arr[i])
             {
-                dp[i][1]= 1+dp[i-1][0];
-                dp[i][0]=1;
+                curr[1]= 1+dp[0];
+                curr[0]=1;
             }
             else if(arr[i-1]<arr[i])
             {
-                dp[i][0]=1+dp[i-1][1];
-                dp[i][1]=1;
+                curr[0]=1+dp[1];
+                curr[1]=1;
             }
             else
             {
-                dp[i][0]=1;
-                dp[i][1]=1;
+                curr[0]=1;
+                curr[1]=1;
             }
+              maxi = Math.max(maxi, curr[0]);
+            maxi = Math.max(maxi, curr[1]);
+            dp=curr;
         }
-        int maxi = 1;
-        for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, dp[i][0]);
-            maxi = Math.max(maxi, dp[i][1]);
-        }
+
         return maxi;
     
     }
