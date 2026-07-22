3c6aa4953c06ffb0cede5be9d8850bc02ce31e14
Aayush Kumar Gupta <ayushkmr2509@gmail.com>
2026-07-22 12:46:04 +0530

    Time: 12 ms (7.28%) | Memory: 54.9 MB (98.54%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.java
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.java
@@ -4,44 +4,15 @@ class Solution {
         int n = arr.length;
         int ans = Integer.MIN_VALUE;
         dp = new int[n][2];
-        for(int []it:dp)
+        dp[0][0]=arr[0];
+        dp[0][1]=(int)-1e9;
+        ans = dp[0][0];
+        for(int i=1;i<n;i++)
         {
-            Arrays.fill(it,-1);
-        }
-        for(int i=0;i<n;i++)
-        {
-        int nodelete = count(arr,i,0);
-        int onedelete = count(arr,i,1);
-        ans = Math.max(ans,Math.max(nodelete,onedelete));
+            dp[i][0]= Math.max(arr[i],arr[i]+dp[i-1][0]);
+            dp[i][1]= Math.max(dp[i-1][0],arr[i]+dp[i-1][1]);
+            ans = Math.max(ans,Math.max(dp[i][0],dp[i][1]));
         }
         return ans;
-    }
-    private int count(int []arr,int index,int up)
-    {
-        if(index==0)
-        {
-            if(up==0)
-            {
-                return arr[index];
-            }
-            else
-            {
-                return (int)-1e9;
-            }
-        }
-        if(dp[index][up]!=-1)
-        {
-            return dp[index][up];
-        }
-        if(up==0)
-        {
-          return dp[index][up]=Math.max(arr[index],arr[index]+count(arr,index-1,0));
-        }
-        else
-        {
-            int keep = count(arr,index-1,0);
-            int nonkeep = arr[index]+count(arr,index-1,1);
-            return dp[index][up]= Math.max(keep,nonkeep);
-        }
     }
 }
