a205b1a45d940b3354a32012d4e92619001e1338
Aayush Kumar Gupta <ayushkmr2509@gmail.com>
2026-08-12 10:31:53 +0530

    Time: 210 ms (5.34%) | Memory: 150.5 MB (17.36%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -5,22 +5,20 @@ public:
         int i = 0;
         int j = 0;
         int ans = INT_MIN;
-        map<int,int>mp;
+        map<int, int> mp;
         while (j < n) {
             mp[nums[j]]++;
 
-           if(mp[nums[j]]<=k)
-           {
-            ans = max(ans,j-i+1);
-            j++;
-           }
-             else {
-                while (mp[nums[j]]>k) {
+            if (mp[nums[j]] <= k) {
+                ans = max(ans, j - i + 1);
+                j++;
+            } else {
+                while (mp[nums[j]] > k) {
                     mp[nums[i]]--;
                     i++;
-                    }
-                    j++;
                 }
+                j++;
+            }
         }
         return ans;
     }
