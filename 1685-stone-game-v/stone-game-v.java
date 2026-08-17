c907ee25ce414cd6b006916c6282381355be3299
Aayush Kumar Gupta <ayushkmr2509@gmail.com>
2026-08-17 16:17:49 +0530

    Time: 236 ms (80.53%) | Memory: 47.8 MB (49.28%) - LeetSync

--- 1685-stone-game-v/stone-game-v.java
+++ 1685-stone-game-v/stone-game-v.java
@@ -7,39 +7,47 @@ class Solution {
         {
             Arrays.fill(it,-1);
         }
-        return game(stoneValue, 0, n - 1);
+        int []prefixsum = new int [n];
+        prefixsum[0]=stoneValue[0];
+        for(int i=1;i<n;i++)
+        {
+            prefixsum[i]=prefixsum[i-1]+stoneValue[i];
+        }
+        return game(stoneValue,0,n-1,prefixsum);
     }
-
-    private int game(int[] arr, int i, int j) {
-        if (i == j) {
+    private int game(int []stoneValue,int i, int j,int []prefixsum)
+    {
+        if(i==j)
+        {
             return 0;
         }
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-        int total = 0;
-        for (int w = i; w <= j; w++) {
-            total += arr[w];
-        }
-        int sum = 0;
-        int ans = Integer.MIN_VALUE;
-        for (int index = i; index <j; index++) {
-            sum += arr[index];
-            int rem = total - sum;
-            if (sum > rem) {
-                int step = rem + game(arr, index+1, j);
-                ans = Math.max(ans, step);
-            } else if (rem > sum) {
-                int step = sum + game(arr, i, index);
-                ans = Math.max(ans, step);
-            } else {
-                int step = rem + game(arr, i, index);
-                int nontake = rem + game(arr, index+1, j);
-                ans = Math.max(ans, Math.max(step, nontake));
+         int sum =0;
+         int result =0;
+        for(int k= i;k<j;k++)
+        {
+            sum+= stoneValue[k];
+            int rem = prefix(k,j,prefixsum);
+            if(sum>rem)
+            {
+            result = Math.max(result,rem+game(stoneValue,k+1,j,prefixsum));
             }
-
+            else if(rem>sum)
+            {
+              result= Math.max(result,sum+ game(stoneValue,i,k,prefixsum));
+            }
+            else 
+            {
+                result = Math.max(result,Math.max(game(stoneValue,k+1,j,prefixsum),game(stoneValue,i,k,prefixsum))+sum);
+            }
         }
-        return dp[i][j]=ans;
+        return dp[i][j]= result;
+    }
+    private int prefix(int l,int r,int []prefix)
+    {
+       return prefix[r]-prefix[l];
     }
 }
