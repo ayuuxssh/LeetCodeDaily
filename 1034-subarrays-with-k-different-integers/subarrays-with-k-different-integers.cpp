f8af61304bac3416f9a24999bf47864f14c925d9
Aayush Kumar Gupta <ayushkmr2509@gmail.com>
2026-08-25 10:43:19 +0530

    Time: 207 ms (5.57%) | Memory: 69.4 MB (7.04%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -2,44 +2,37 @@ class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         int n = nums.size();
-       int ans = getans(nums,k);
-       int ans1 = getans(nums,k-1);
-       return ans-ans1;
+        int ans = getans(nums, k);
+        int ans1 = getans(nums, k - 1);
+        return ans - ans1;
     }
-    private:
-    int getans(vector<int>&nums,int k)
-    {
+
+private:
+    int getans(vector<int>& nums, int k) {
         int n = nums.size();
-        if(k<0)
-        {
+        if (k < 0) {
             return 0;
         }
-         unordered_set<int>st;
-        map<int,int>mp;
-        int i=0;
-        int j=0;
-        int ans=0;
-        while(j<n)
-        {
+        unordered_set<int> st;
+        map<int, int> mp;
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        while (j < n) {
             mp[nums[j]]++;
             st.insert(nums[j]);
-             if(st.size()<=k)
-            {
-                ans+=j-i+1;
+            if (st.size() <= k) {
+                ans += j - i + 1;
                 j++;
-            }
-            else
-            {
-                while(st.size()>k)
-                {
+            } else {
+                while (st.size() > k) {
                     mp[nums[i]]--;
-                    if(mp[nums[i]]==0)
-                    {
+                    if (mp[nums[i]] == 0) {
                         st.erase(nums[i]);
                     }
                     i++;
                 }
-                ans+=j-i+1;
+                ans += j - i + 1;
                 j++;
             }
         }
