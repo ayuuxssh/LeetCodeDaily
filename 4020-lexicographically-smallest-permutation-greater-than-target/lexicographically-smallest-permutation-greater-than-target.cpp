abd1637a1bd31b6a1ab049d256bcdd427a776f3b
Aayush Kumar Gupta <ayushkmr2509@gmail.com>
2026-08-27 21:41:59 +0530

    Time: 1 ms (77.07%) | Memory: 9.8 MB (82.17%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
-    
-    bool solve(string&target,vector<int>freq,int index,bool greater,string &ans,string &result)
-    {
-        if(index==target.length())
-        {
-            if(greater)
-            {
-                ans =result;
+    bool solve(string& target, vector<int>& freq, int index, bool greater,
+               string& ans, string& result) {
+        if (index == target.length()) {
+            if (greater) {
+                ans = result;
                 return true;
             }
             return false;
         }
 
-        for(char c='a';c<='z';c++)
-        {
-            if(freq[c-'a']==0)
-            {
-              continue;
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (freq[c - 'a'] == 0) {
+                continue;
             }
-            if(greater==false && c<target[index])
-            {
+            if (greater == false && c < target[index]) {
                 continue;
             }
             result.push_back(c);
-            freq[c-'a']--;
-             bool isGreater = greater || c>target[index];
-            if(solve(target,freq,index+1,isGreater,ans,result))
-            {
+            freq[c - 'a']--;
+            bool isGreater = greater || c > target[index];
+            if (solve(target, freq, index + 1, isGreater, ans, result)) {
                 return true;
             }
             result.pop_back();
-            freq[c-'a']++;
+            freq[c - 'a']++;
         }
         return false;
     }
     string lexGreaterPermutation(string s, string target) {
-        vector<int>freq(26,0);
+        vector<int> freq(26, 0);
         int n = s.length();
-        for(auto &it:s)
-        {
-            freq[it-'a']++;
+        for (auto& it : s) {
+            freq[it - 'a']++;
         }
-        string ans="";
+        string ans = "";
         string result;
-        solve(target,freq,0,false,ans,result);
+        solve(target, freq, 0, false, ans, result);
         return ans;
     }
 };
